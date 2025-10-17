bdb66cc14d3993fdf9181df58ab150eb81f9c2e2
WhoLuNeX <lunaxxxx691@gmail.com>
2025-10-17 15:02:33 +0000

    $

--- script.js
+++ script.js
@@ -25,3 +25,73 @@ const parler = (nom) => {
 };
 const saluer = parler(nom);
 console.log(saluer)
+
+
+
+// Pseudo Code :
+//  1. Formater des données brutes correctement pour les afficher à l'utilisateur.
+// 2.   Prénom en minuscules de base, résultat on doit les retourner pour qu'il donne les prénom en avec une majuscules au début
+// nom.slice =  Le reste du mot ('lice') peut être obtenu avec
+// Pour appliquer cette logique à chaque nom du tableau, la méthode .map() est parfaite.
+
+const nom = ["alice","bob","charlie"];
+
+  const nom1 = ["alice","bob","charlie"];
+
+
+const nameToUpperCase = (names) =>{
+    for (let i = 0; i < names.length; i++) {
+        const name = names[i]
+        nom.split()
+        nom(0) toUpperCase(0) + nom.slice(1)
+    } 
+}
+
+        
+let monTableau = maRegexp.exec("Alice");
+let monTableau = maRegexp.exec("Bob");
+let monTableau = maRegexp.exec("Charlie");
+}
+console.log(monTableau);
+
+console.log(filtreTexte(names, "a")); // ['alice'];
+console.log(filtreTexte(names, "b")); // ['bob'];
+console.log(filtreTexte(names, "c")); // ['Charlie'];
+
+
+
+        console.log(nom.length);
+
+    let first = nom[0];
+
+    let last = nom[nom.length - 2];
+
+const words = ["alice", "bob", "charlie"];
+
+const result = words.filter((word) => word.length > 3);
+
+console.log(result);
+
+arr.filter(callback); 
+var nouveauTableau = arr.filter(callback, thisArg);
+
+var nom = ["alice", "bob", "charlie"];
+
+function filtreTexte(arr, requete) {
+  return arr.filter(function (el) {
+    return el.toLowerCase().indexOf(requete.toUpperCase()) !== -1;
+  });
+}
+
+console.log(filtreTexte(fruits, "a")); // ['alice'];
+console.log(filtreTexte(fruits, "b")); // ['bob'];
+console.log(filtreTexte(fruits, "c")); // ['Charlie'];
+
+arr.slice();
+arr.slice(0);
+arr.slice(0, 2);
+
+
+var nom = ["Alice", "Bob", "Charlie"];
+var .map() = nom.slice(0, 2);
+
